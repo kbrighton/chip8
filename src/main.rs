67d83b651d1f013413c75c9215ca42582d969985
Nick Burtner <cstrife@gmail.com>
2023-10-24 04:23:18 -0400

    Fixed formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::fs::File;
-use std::io::Read;
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
 use sdl2::keyboard::Keycode::H;
+use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use std::fs::File;
+use std::io::Read;
+use std::time::Duration;
 
 static SP_OFFSET: u16 = 0;
 static PROGRAM_OFFSET: u16 = 0x200;
@@ -39,9 +39,8 @@ const FONTSET: [u8; FONTSET_SIZE] = [
     0xF0, 0x80, 0x80, 0x80, 0xF0, // C
     0xE0, 0x90, 0x90, 0x90, 0xE0, // D
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
-
 
 struct Chip8 {
     registers: Registers,
@@ -50,7 +49,7 @@ struct Chip8 {
     screen: [bool; WIDTH * HEIGHT],
     running: bool,
     operand: u16,
-    stack: [u16; STACK_SIZE]
+    stack: [u16; STACK_SIZE],
 }
 
 struct Registers {
@@ -81,7 +80,7 @@ impl Chip8 {
             screen: [false; WIDTH * HEIGHT],
             running: true,
             operand: 0,
-            stack: [0; STACK_SIZE]
+            stack: [0; STACK_SIZE],
         };
         emu.memory[..FONTSET_SIZE].copy_from_slice(&FONTSET);
 
@@ -138,118 +137,106 @@ impl Chip8 {
         let op3 = (operation & 0x00F0) >> 4;
         let op4 = (operation & 0x000F);
 
-        match(op1, op2, op3, op4) {
-            (0,0,0,0) => return,
-            (0,0,0xE,0) => {
+        match (op1, op2, op3, op4) {
+            (0, 0, 0, 0) => return,
+            (0, 0, 0xE, 0) => {
                 self.screen = [false; WIDTH * HEIGHT];
-            },
-            (0,0,0xE,0xE) => {
+            }
+            (0, 0, 0xE, 0xE) => {
                 let return_addr = self.pop();
                 self.registers.pc = return_addr;
-            },
+            }
             (1, _, _, _) => {
-              let nnn = operation & 0xFFF;
-              self.registers.pc = nnn;
-            },
-            (2,_,_,_) => {
+                let nnn = operation & 0xFFF;
+                self.registers.pc = nnn;
+            }
+            (2, _, _, _) => {
                 let nnn = operation & 0xFFF;
                 self.push(self.registers.pc);
                 self.registers.pc = nnn;
-            },
-            (3,_,_,_) => {
+            }
+            (3, _, _, _) => {
                 let x = op2 as usize;
                 let nn = (operation & 0xFF) as u8;
-                if( self.registers.v[x] == nn) {
+                if (self.registers.v[x] == nn) {
                     self.registers.pc += 2;
                 }
-            },
-            (4,_,_,_) => {
+            }
+            (4, _, _, _) => {
                 let x = op2 as usize;
                 let nn = (operation & 0xFF) as u8;
-                if( self.registers.v[x] != nn) {
+                if (self.registers.v[x] != nn) {
                     self.registers.pc += 2;
                 }
-            },
-            (5,_,_,_) => {
+            }
+            (5, _, _, _) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 if self.registers.v[x] == self.registers.v[y] {
                     self.registers.pc += 2;
                 }
-            },
+            }
             (6, _, _, _) => {
                 let x = op2 as usize;
                 let nn = (operation & 0xFF) as u8;
                 self.registers.v[x] = nn;
-            },
+            }
             (7, _, _, _) => {
                 let x = op2 as usize;
                 let nn = (operation & 0xFF) as u8;
                 self.registers.v[x] = self.registers.v[x].wrapping_add(nn);
-            },
+            }
             (8, _, _, 0) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 self.registers.v[x] = self.registers.v[y];
-            },
+            }
             (8, _, _, 1) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 self.registers.v[x] |= self.registers.v[y];
-            },
+            }
             (8, _, _, 2) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 self.registers.v[x] &= self.registers.v[y];
-            },
+            }
             (8, _, _, 3) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 self.registers.v[x] ^= self.registers.v[y];
-            },
+            }
             (8, _, _, 4) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 let (new_x, carry) = self.registers.v[x].overflowing_add(self.registers.v[y]);
-                let new_f = if carry {
-                    1
-                } else {
-                    0
-                };
+                let new_f = if carry { 1 } else { 0 };
                 self.registers.v[x] = new_x;
-                self.registers.v[0xF]= new_f;
-            },
+                self.registers.v[0xF] = new_f;
+            }
             (8, _, _, 5) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 let (new_x, borrow) = self.registers.v[x].overflowing_sub(self.registers.v[y]);
-                let new_f = if borrow {
-                    0
-                } else {
-                    1
-                };
+                let new_f = if borrow { 0 } else { 1 };
                 self.registers.v[x] = new_x;
-                self.registers.v[0xF]= new_f;
-            },
+                self.registers.v[0xF] = new_f;
+            }
             (8, _, _, 6) => {
                 let x = op2 as usize;
                 let lsb = self.registers.v[x] & 1;
 
                 self.registers.v[x] >>= 1;
                 self.registers.v[0xF] = lsb;
-            },
+            }
             (8, _, _, 7) => {
                 let x = op2 as usize;
                 let y = op3 as usize;
                 let (new_x, borrow) = self.registers.v[y].overflowing_sub(self.registers.v[x]);
-                let new_f = if borrow {
-                    0
-                } else {
-                    1
-                };
+                let new_f = if borrow { 0 } else { 1 };
                 self.registers.v[x] = new_x;
-                self.registers.v[0xF]= new_f;
-            },
+                self.registers.v[0xF] = new_f;
+            }
 
             (8, _, _, 0xE) => {
                 let x = op2 as usize;
@@ -257,22 +244,22 @@ impl Chip8 {
 
                 self.registers.v[x] <<= 1;
                 self.registers.v[0xF] = msb;
-            },
-            (9,_,_,0) => {
+            }
+            (9, _, _, 0) => {
                 let x = op2 as usize;
                 let y = op2 as usize;
-                if( self.registers.v[x] != self.registers.v[y]) {
+                if (self.registers.v[x] != self.registers.v[y]) {
                     self.registers.pc += 2;
                 }
-            },
+            }
             (0xA, _, _, _) => {
                 let nnn = operation & 0xFFF;
                 self.registers.index = nnn;
-            },
+            }
             (0xB, _, _, _) => {
                 let nnn = operation & 0xFFF;
                 self.registers.pc = (self.registers.v[0] as u16) + nnn;
-            },
+            }
             (0xD, _, _, _) => {
                 let x_coord = self.registers.v[op2 as usize] as u16;
                 let y_coord = self.registers.v[op3 as usize] as u16;
@@ -286,7 +273,7 @@ impl Chip8 {
                     let pixels = self.memory[addr as usize];
 
                     for x_line in 0..8 {
-                        if(pixels & (0b1000_0000 >> x_line)) != 0 {
+                        if (pixels & (0b1000_0000 >> x_line)) != 0 {
                             let x = (x_coord + x_line) as usize % WIDTH;
                             let y = (y_coord + y_line) as usize % HEIGHT;
 
@@ -301,12 +288,12 @@ impl Chip8 {
                 } else {
                     self.registers.v[0xF] = 0;
                 }
-            },
-            (0xF,_,3,3) => {
+            }
+            (0xF, _, 3, 3) => {
                 let x = op2 as usize;
                 let v = self.registers.v[x] as f32;
 
-                let hundreds = (v/100.0).floor() as u8;
+                let hundreds = (v / 100.0).floor() as u8;
                 let tens = ((v / 10.0) % 10.0).floor() as u8;
                 let ones = (v % 10.0) as u8;
 
@@ -314,20 +301,20 @@ impl Chip8 {
                 self.memory[(self.registers.index + 1) as usize] = tens;
                 self.memory[(self.registers.index + 2) as usize] = ones;
             }
-            (0xF,_, 5,5) => {
+            (0xF, _, 5, 5) => {
                 let x = op2 as usize;
                 let i = self.registers.index as usize;
                 for index in 0..=x {
                     self.memory[i + index] = self.registers.v[index];
                 }
-            },
-            (0xF,_, 6,5) => {
+            }
+            (0xF, _, 6, 5) => {
                 let x = op2 as usize;
                 let i = self.registers.index as usize;
                 for index in 0..=x {
                     self.registers.v[index] = self.memory[i + index];
                 }
-            },
+            }
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {:#04x}", operation),
         }
     }
@@ -338,9 +325,7 @@ impl Chip8 {
         }
 
         if self.timers.sound > 0 {
-            if self.timers.sound == 1 {
-
-            }
+            if self.timers.sound == 1 {}
             self.timers.sound -= 1;
         }
     }
@@ -378,10 +363,13 @@ fn main() -> Result<(), String> {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
-                },
+                }
                 _ => {}
             }
         }
@@ -391,19 +379,17 @@ fn main() -> Result<(), String> {
         }
         chip.update_timer();
         update_screen(&chip, &mut canvas);
-
     }
 
     Ok(())
 }
 
-
 fn update_screen(emu: &Chip8, canvas: &mut Canvas<Window>) {
-    canvas.set_draw_color(Color::RGB(0,0,0));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
 
     let screen_buf = emu.get_screen_buf();
-    canvas.set_draw_color(Color::RGB(255,255,255));
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
     for (i, pixel) in screen_buf.iter().enumerate() {
         if *pixel {
             let x = (i % WIDTH) as u32;
